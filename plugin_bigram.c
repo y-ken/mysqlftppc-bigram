9597f4e43520ef93802051ee444962e4fd01e691
hiroaki-kawai <hiroaki-kawai@fbe6fafc-a77a-4c0b-9ca6-bdabefc0bdc6>
2008-08-07 10:32:25 +0000

    fix operator
    git-svn-id: http://mysqlftppc.svn.sourceforge.net/svnroot/mysqlftppc/bigram/trunk@15 fbe6fafc-a77a-4c0b-9ca6-bdabefc0bdc6

--- plugin_bigram.c
+++ plugin_bigram.c
@@ -149,7 +149,7 @@ static int bigram_parser_parse(MYSQL_FTPARSER_PARAM *param)
       int c,mark;
       uint t_res= uc->sort_order_big[0][0x20 * uc->sort_order[0]];
       for(mark=0,c=0; c<wlen; c+=2){
-        if(*(w_buffer+c) == t_res>>8 && *(w_buffer+c+1) == t_res&0xFF){
+        if((*(w_buffer+c) == (t_res>>8)) && (*(w_buffer+c+1) == (t_res&0xFF))){
           // it is space or padding.
         }else{
           mark = c;
