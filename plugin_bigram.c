b0fbc16b1a2fc7a5be355791a153efdf399dca8a
hiroaki-kawai <hiroaki-kawai@fbe6fafc-a77a-4c0b-9ca6-bdabefc0bdc6>
2008-08-07 11:00:45 +0000

    prepare for release
    git-svn-id: http://mysqlftppc.svn.sourceforge.net/svnroot/mysqlftppc/bigram/trunk@17 fbe6fafc-a77a-4c0b-9ca6-bdabefc0bdc6

--- plugin_bigram.c
+++ plugin_bigram.c
@@ -211,7 +211,7 @@ mysql_declare_plugin(ft_bigram)
   PLUGIN_LICENSE_BSD,
   bigram_parser_plugin_init,  /* init function (when loaded)     */
   bigram_parser_plugin_deinit,/* deinit function (when unloaded) */
-  0x0001,                     /* version                         */
+  0x0010,                     /* version                         */
   NULL,                       /* status variables                */
   NULL,                       /* system variables                */
   NULL
