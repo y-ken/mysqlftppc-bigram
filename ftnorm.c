a861d6ac4bfe9c4d26d24a4f7d3a35d3b0b6979a
hiroaki-kawai <hiroaki-kawai@fbe6fafc-a77a-4c0b-9ca6-bdabefc0bdc6>
2009-01-26 04:52:25 +0000

    bugfix
    git-svn-id: http://mysqlftppc.svn.sourceforge.net/svnroot/mysqlftppc/bigram/trunk@100 fbe6fafc-a77a-4c0b-9ca6-bdabefc0bdc6

--- ftnorm.c
+++ ftnorm.c
@@ -4,16 +4,12 @@
 #include <unicode/unorm.h>
 #include <unicode/ustring.h>
 
+#include "ftnorm.h"
+
 /**
- * @param src source string
- * @param src_len source string length
- * @param dst buffer to write
- * @param dst_capacity buffer capacity
  * @param mode same with ICU mode flag UNormalizationMode.
- * @param options same with ICU options flag
- * @return buffer size required. 0 on failure. When the value was larger than dst_capacity, you must realloc and retry.
  */
-size_t uni_normalize(char* src, size_t src_len, char* dst, size_t dst_capacity, int mode, int options){
+size_t uni_normalize(char* src, size_t src_len, char* dst, size_t dst_capacity, int mode, int opt){
     UNormalizationMode umode = (UNormalizationMode)mode;
     // status holder
     UErrorCode ustatus=0;
@@ -36,10 +32,10 @@ size_t uni_normalize(char* src, size_t src_len, char* dst, size_t dst_capacity, 
 			return 0;
     }
     ustatus = 0;
-    u_strFromUTF8(s, s_alloc, NULL, src, (int32_t)src_len, &ustatus);
+    s = u_strFromUTF8(s, s_alloc, NULL, src, (int32_t)src_len, &ustatus);
     if(U_FAILURE(ustatus)){
 			ustatus = 0;
-			s_alloc = u_strFromUTF8(NULL, 0, NULL, src, (int32_t)src_len, &ustatus) + 4; // prefill
+			s_alloc = (size_t)u_strFromUTF8(NULL, 0, NULL, src, (int32_t)src_len, &ustatus) + 4; // prefill
       tmp = (UChar*)my_realloc(s, s_alloc, MYF(MY_WME));
       if(!tmp){
 				my_free(s, MYF(0));
@@ -48,7 +44,7 @@ size_t uni_normalize(char* src, size_t src_len, char* dst, size_t dst_capacity, 
 				s=tmp;
 			}
       ustatus = 0;
-      u_strFromUTF8(s, s_alloc, NULL, src, (int32_t)src_len, &ustatus);
+      s = u_strFromUTF8(s, s_alloc, NULL, src, (int32_t)src_len, &ustatus);
       if(U_FAILURE(ustatus)){
 				my_free(s, MYF(0));
 				return 0;
@@ -60,7 +56,7 @@ size_t uni_normalize(char* src, size_t src_len, char* dst, size_t dst_capacity, 
     if(!d){
 			return 0;
     }
-    d_len = unorm_normalize(s, -1, umode, (int32_t)options, d, d_alloc, &ustatus);
+    d_len = unorm_normalize(s, -1, umode, (int32_t)opt, d, d_alloc, &ustatus);
     if(ustatus == U_BUFFER_OVERFLOW_ERROR){
 				d_alloc = d_len + 8;
         tmp = (UChar*)my_realloc(d, d_alloc, MYF(MY_WME));
@@ -71,18 +67,15 @@ size_t uni_normalize(char* src, size_t src_len, char* dst, size_t dst_capacity, 
 					d=tmp;
 				}
         ustatus = 0;
-        d_len = unorm_normalize(s, -1, mode, (int32_t)options, d, d_alloc, &ustatus);
+        d_len = unorm_normalize(s, -1, mode, (int32_t)opt, d, d_alloc, &ustatus);
     }
     my_free(s, MYF(0));
     // encode UChar -> UTF-8
     u_strToUTF8(dst, (int32_t)dst_capacity, NULL, d, d_len, &ustatus);
-    if(U_FAILURE(ustatus)){ // U_BUFFER_OVERFLOW_ERROR
-			ustatus = 0;
-			dst_alloc = u_strToUTF8(NULL, 0, NULL, d, d_len, &ustatus);
-	    my_free(d, MYF(0));
-			return dst_alloc;
-    }
     my_free(d, MYF(0));
+    if(ustatus == U_BUFFER_OVERFLOW_ERROR){
+			return (size_t)u_strToUTF8(NULL, 0, NULL, d, d_len, &ustatus);
+    }
 		return strlen(dst);
 }
 #endif
